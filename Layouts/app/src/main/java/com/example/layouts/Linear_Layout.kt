a95e1745e2a8e23926db3dd4cf2c4c7de781d043
vicsurcor <vdsurcor@gmail.com>
2024-01-15 17:35:11 +0100

    Fin

--- Layouts/app/src/main/java/com/example/layouts/Linear_Layout.kt
+++ Layouts/app/src/main/java/com/example/layouts/Linear_Layout.kt
@@ -1,11 +1,33 @@
 package com.example.layouts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class Linear_Layout: AppCompatActivity() {
+    private lateinit var correo:EditText;
+    private lateinit var contrasena: EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.linear_act2)
+
+        correo = findViewById(R.id.correo)
+        contrasena = findViewById(R.id.contrasena)
+
+        fun clearFieldOnFocusChange(v: View, hasFocus: Boolean) {
+            if (hasFocus) {
+
+                (v as EditText).apply {
+
+                    text.clear();
+
+                }
+            }
+        }
+        val fields = listOf(correo, contrasena)
+        fields.forEach { it.setOnFocusChangeListener(::clearFieldOnFocusChange)
+    }
     }
 }
