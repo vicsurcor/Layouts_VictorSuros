7e255c602574125d1b764ac5feffaa716db5fc03
vicsurcor <vdsurcor@gmail.com>
2024-01-15 16:07:51 +0100

    Pagina 1

---
+++ Layouts/app/src/main/java/com/example/layouts/Linear_Layout.kt
@@ -0,0 +1,11 @@
+package com.example.layouts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Linear_Layout: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_act2)
+    }
+}
--- Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.layouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var botonNext: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonNext = findViewById(R.id.botonInferior)
+        botonNext.setOnClickListener {
+            val intent = Intent(this, Linear_Layout::class.java)
+            startActivity(intent)
+        }
     }
 }
